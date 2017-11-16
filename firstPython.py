c0db1870526b3e3bfb7be577ca34055a8209fc86
bomelon <b_melon@163.com>
2017-11-16 15:27:00 +0800

    push

---
+++ firstPython.py
@@ -0,0 +1,22 @@
+print("hello frist python")
+print("joker")
+a=0
+if(a>0):
+    print(a)
+else:
+    print(0)
+str1="asdf"
+str2="bdaxss"
+temp=str1+\
+    str2
+print (temp)
+num = 100
+string = "asdfas"
+dubbo = 123.00012
+float = 12312.123123
+print(num)
+print(string)
+print(dubbo)
+print(float)
+int =1
+print (int)
